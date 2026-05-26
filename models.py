d7d283a882fb3f7f1d90ad2a48bdda5da6128283
Seuko <francisco.contreras.ra@gmail.com>
2026-05-26 19:08:03 -0400

    se agregan modelos y definicion de modelos

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-class MultiLayerPerceptron(nn.Module):
+class MultiLayerPerceptronSig(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, output_dim): 
         super(type(self), self).__init__()  
@@ -12,3 +12,16 @@ class MultiLayerPerceptron(nn.Module):
     def forward(self, x):
         x = self.activation(self.hidden(x))
         return self.output(x)
+
+class MultiLayerPerceptronRelu(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, output_dim): 
+        super(type(self), self).__init__()  
+        self.hidden = nn.Linear(input_dim, hidden_dim)
+        self.output = nn.Linear(hidden_dim, output_dim)        
+        self.activation = nn.ReLU()
+        
+    def forward(self, x):
+        x = self.activation(self.hidden(x))
+        return self.output(x)
+    
