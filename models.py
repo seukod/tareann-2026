2fd8e93017c8bfe8ed7019d791a30b9380862bf8
Seuko <francisco.contreras.ra@gmail.com>
2026-05-27 17:53:14 -0400

    Mejor version: problema ruido con el entrenamiento

--- models.py
+++ models.py
@@ -12,19 +12,8 @@ class MultiLayerPerceptronSig(nn.Module):
     def forward(self, x):
         x = self.activation(self.hidden(x))
         return self.output(x)
-#Arquitectura 2
-class MultiLayerPerceptronRelu(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim, output_dim): 
-        super(type(self), self).__init__()  
-        self.hidden = nn.Linear(input_dim, hidden_dim)
-        self.output = nn.Linear(hidden_dim, output_dim)        
-        self.activation = nn.ReLU()
-        
-    def forward(self, x):
-        x = self.activation(self.hidden(x))
-        return self.output(x)
-#Arquitectura 3
+#Arquitectura 2
 class MultiLayerPerceptron2Cap(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim): 
         super().__init__()  
@@ -37,4 +26,18 @@ class MultiLayerPerceptron2Cap(nn.Module):
     def forward(self, x):
         x = self.activation(self.hidden1(x))
         x = self.activation(self.hidden2(x))
+        return self.output(x)
+    
+    #Arquitectura 3
+class MultiLayerPerceptronRelu(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout_p): 
+        super(type(self), self).__init__()  
+        self.hidden = nn.Linear(input_dim, hidden_dim)
+        self.output = nn.Linear(hidden_dim, output_dim)        
+        self.activation = nn.ReLU()
+        self.dropout = nn.Dropout(p=dropout_p)  
+
+    def forward(self, x):
+        x = self.activation(self.hidden(x))
+        x = self.dropout(x)  
         return self.output(x)
