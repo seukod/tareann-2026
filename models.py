0f940029729c958d65471ca666806d22e941b805
Seuko <francisco.contreras.ra@gmail.com>
2026-05-27 14:54:35 -0400

    agrega 3 modelos

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+#Arquitecutra 1
 class MultiLayerPerceptronSig(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, output_dim): 
@@ -12,7 +12,7 @@ class MultiLayerPerceptronSig(nn.Module):
     def forward(self, x):
         x = self.activation(self.hidden(x))
         return self.output(x)
-
+#Arquitectura 2
 class MultiLayerPerceptronRelu(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, output_dim): 
@@ -24,4 +24,17 @@ class MultiLayerPerceptronRelu(nn.Module):
     def forward(self, x):
         x = self.activation(self.hidden(x))
         return self.output(x)
-    
+#Arquitectura 3
+class MultiLayerPerceptron2Cap(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim): 
+        super().__init__()  
+        self.hidden1 = nn.Linear(input_dim, hidden_dim)
+        # Una segunda capa oculta que mantiene la misma dimensión
+        self.hidden2 = nn.Linear(hidden_dim, hidden_dim)
+        self.output = nn.Linear(hidden_dim, output_dim)        
+        self.activation = nn.Sigmoid() # Puedes usar ReLU o Sigmoide aquí
+        
+    def forward(self, x):
+        x = self.activation(self.hidden1(x))
+        x = self.activation(self.hidden2(x))
+        return self.output(x)
